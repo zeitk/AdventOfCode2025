d9ac625ccc05da473ed479ac00066d5cf4339674
Zeit Kellett <zeitkellett@microsoft.com>
2025-12-03 10:26:05 -0800

    Move input folder location

--- Utils/Utils.cs
+++ Utils/Utils.cs
@@ -5,9 +5,10 @@
         public static string[] GetInput(int day)
         {
             string folderName = "AdventOfCode2025";
-            string curDirectory = Directory.GetCurrentDirectory().Split($@"\{folderName}")[0];
-            string dayName = $"Day{day.ToString()}";
-            string filePath = @$"{curDirectory}\{folderName}\InputFiles\{dayName}.txt";
+            string curDirectory = Directory.GetCurrentDirectory();
+            int index = curDirectory.LastIndexOf(folderName);
+            string projectRoot = curDirectory.Substring(0, index + folderName.Length);
+            string filePath = Path.Combine(projectRoot, "InputFiles", $"Day{day}.txt");
             string[] lines = File.ReadAllLines(filePath);
             return lines;
         }
